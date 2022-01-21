a3549bcbeb6f1340474720cbd7be18087b7776bc
isaquelemos <isaquelemos_bds@hotmail.com>
2022-01-21 18:37:31 -0300

    Variaiveis

---
+++ Capitulo1/Variaveis/script.js
@@ -0,0 +1,16 @@
+var num = 2; // Declarando uma nova variável
+num = 3; // Reatribuindo o valor de uma variável
+var price = 1.5; // Declarando um número Float
+var myName = 'Isaque'; // Declarando uma String
+var trueValue = true; // Declarando um Booleano
+var nullVar = null; // Declarando um valor nulo
+var und; // Criando um variável sem valor, indefinida
+
+console.log('num: ' + num);
+console.log('myName: ' + myName);
+console.log('trueValue: ' + trueValue);
+console.log('price: ' + price);
+console.log('nullVar: ' + nullVar);
+console.log('und: ' + und);
+// Outra forma
+console.log('num:', num);
