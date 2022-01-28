bf3df441b53130869d75dd141b5bed89aec1a12f
isaquelemos <isaquelemos_bds@hotmail.com>
2022-01-28 16:19:46 -0300

    Laços

---
+++ Capitulo1/Laços/script.js
@@ -0,0 +1,24 @@
+/* Usando as instruções de repetição FOR e WHILE */
+
+// FOR
+for (let i = 0; i < 10; i++) { // recebe um valor ; compara ; incrementa/decrementa
+    console.log('Number', i);
+}
+
+console.log('\n');
+
+// WHILE, Condição é avaliada antes da execução do bloco de código.
+let i = 1
+while (i <= 10) {
+    console.log('Grade', i);
+    i++;
+}
+
+console.log('\n');
+
+// DO - WHILE, Condição é avaliada depois de o bloco de código ter sido executado.
+i = 0
+do {
+    console.log('Position', i);
+    i++;
+} while (i < 10);
