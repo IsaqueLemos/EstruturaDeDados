4f892145d66e0c773e470c6a134fd34181b23b83
isaquelemos <isaquelemos_bds@hotmail.com>
2022-02-09 19:03:57 -0300

    Inserindo elemento no final do array

---
+++ Capitulo3/Acrescentando elementos/script.js
@@ -0,0 +1,5 @@
+/* Acrescentando Elementos */
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+/* Inserindo elemento no final do array */
+numbers[numbers.length] = 10; // numero 10 adicionado no final da Array
