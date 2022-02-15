535085a1003e19c95f693cba41d9409653b235bb
isaquelemos <isaquelemos_bds@hotmail.com>
2022-02-15 11:12:00 -0300

    Utilizando o método concat

---
+++ Capitulo3/Juntando varios arrays/script.js
@@ -0,0 +1,8 @@
+/* Juntando vários arrays */
+
+// Utilizando o método concat
+const zero = 0;
+const positiveNumbers = [1, 2, 3];
+const negativeNumbers = [-3, -2, -1];
+numbers = negativeNumbers.concat(zero, positiveNumbers);
+console.log(numbers);
