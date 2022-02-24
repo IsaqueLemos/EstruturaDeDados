afcc078adea4e7534bf5d46069b4b4922d8da082
isaquelemos <isaquelemos_bds@hotmail.com>
2022-02-24 15:24:21 -0300

    Utilizando os métodos toString e join

---
+++ Capitulo3/Transformando um array numa string/script.js
@@ -0,0 +1,8 @@
+/* Utilizando os métodos toString e Join */
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+// Utilizando toString
+console.log(numbers.toString());
+
+// Utilizando join
+console.log(numbers.join('~..~'));
