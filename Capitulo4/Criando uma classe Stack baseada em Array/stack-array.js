39b25b4de0e07bfaa890f6b2378325600c5f8b09
isaquelemos <isaquelemos_bds@hotmail.com>
2022-03-11 16:00:18 -0300

    Adicionando métodos à classe Stack

--- Capitulo4/Criando uma classe Stack baseada em Array/stack-array.js
+++ Capitulo4/Criando uma classe Stack baseada em Array/stack-array.js
@@ -3,7 +3,31 @@
 
 class Stack {
     constructor() {
-        this.item = [];
+        this.items = [];
+    }
+
+    push(element) {
+        this.items.push(element);
+    }
+
+    pop() {
+        this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    size() {
+        return this.items.length;
+    }
+
+    clear() {
+        this.items = [];
     }
 }
 
